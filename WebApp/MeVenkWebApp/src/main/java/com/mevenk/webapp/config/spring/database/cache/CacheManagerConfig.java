6b51fc742b5c829f0bc8b93b567eb5f86f23ea30
Venkatesh Kolisetty <venkateshkolisetty@users.noreply.github.com>
2018-09-13 07:47:31 +0530

    CacheManager miscellaneous changes

--- WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/config/spring/database/cache/CacheManagerConfig.java
+++ WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/config/spring/database/cache/CacheManagerConfig.java
@@ -1,5 +1,8 @@
 package com.mevenk.webapp.config.spring.database.cache;
 
+import static com.mevenk.webapp.config.logger.MeVenkWebAppLogger.CONFIG;
+import static net.sf.ehcache.CacheManager.getInstance;
+import static net.sf.ehcache.store.MemoryStoreEvictionPolicy.LFU;
 import static org.apache.logging.log4j.LogManager.getLogger;
 
 import javax.annotation.PreDestroy;
@@ -9,42 +12,41 @@ import org.apache.logging.log4j.Logger;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.config.CacheConfiguration;
-import net.sf.ehcache.store.MemoryStoreEvictionPolicy;
 
 public class CacheManagerConfig {
 
 	private static final Logger LOG = getLogger(CacheManagerConfig.class);
+
+	public static final String CACHE_HIBERNATE = "CACHE_HIBERNATE";
+	public static final String CACHE_MEVENK_WEBAPP = "CACHE_MEVENK_WEBAPP";
+
 	private static final int MAX_ELEMENTS_IN_MEMORY = 1000;
 	private static final int DISK_POOL_BUFFER_SIZE_IN_MB = 5;
 
-	private static final String HIBERNATE_CACHE_1 = "HIBERNATE_CACHE_1";
-	private static final String HIBERNATE_CACHE_2 = "HIBERNATE_CACHE_2";
-
 	public CacheManagerConfig() {
-		LOG.debug("Creating Custom Caches");
+		LOG.log(CONFIG, "Creating Custom Caches");
 		// Create a singleton CacheManager using defaults
-		CacheManager manager = CacheManager.getInstance();
+		CacheManager manager = getInstance();
 
-		Cache customCapabilityNameCacheRegion = createCache(HIBERNATE_CACHE_1);
-		Cache customCapabilityIdCacheRegion = createCache(HIBERNATE_CACHE_2);
+		Cache cacheHibernate = createCache(CACHE_HIBERNATE);
+		Cache cacheMeVenkWebApp = createCache(CACHE_MEVENK_WEBAPP);
 
-		LOG.debug("Adding Caches");
-		manager.removeCache(HIBERNATE_CACHE_1);
-		manager.removeCache(HIBERNATE_CACHE_2);
-		manager.addCache(customCapabilityNameCacheRegion);
-		manager.addCache(customCapabilityIdCacheRegion);
+		LOG.log(CONFIG, "Adding Caches");
+		manager.removeCache(CACHE_HIBERNATE);
+		manager.removeCache(CACHE_MEVENK_WEBAPP);
+		manager.addCache(cacheHibernate);
+		manager.addCache(cacheMeVenkWebApp);
 
 		String[] strCacheNames = manager.getCacheNames();
 		for (String cache : strCacheNames) {
-			LOG.debug("Caches Present : {} " + cache);
+			LOG.log(CONFIG, "Caches Present : {} " + cache);
 		}
 
 	}
 
 	private Cache createCache(String cacheName) {
-		Cache cache = new Cache(new CacheConfiguration(cacheName, MAX_ELEMENTS_IN_MEMORY)
-				.memoryStoreEvictionPolicy(MemoryStoreEvictionPolicy.LFU).eternal(true)
-				.maxElementsInMemory(MAX_ELEMENTS_IN_MEMORY).timeToIdleSeconds(0).timeToLiveSeconds(0)
+		Cache cache = new Cache(new CacheConfiguration(cacheName, MAX_ELEMENTS_IN_MEMORY).memoryStoreEvictionPolicy(LFU)
+				.eternal(true).maxElementsInMemory(MAX_ELEMENTS_IN_MEMORY).timeToIdleSeconds(0).timeToLiveSeconds(0)
 				.diskSpoolBufferSizeMB(DISK_POOL_BUFFER_SIZE_IN_MB).overflowToDisk(false)
 
 		);
@@ -55,16 +57,16 @@ public class CacheManagerConfig {
 
 	@PreDestroy
 	public static void cleanUp() {
-		CacheManager.getInstance().shutdown();
+		getInstance().shutdown();
 	}
 
 	public void clearCache() {
 
-		CacheManager manager = CacheManager.getInstance();
-		manager.getCache(HIBERNATE_CACHE_1).removeAll();
-		manager.getCache(HIBERNATE_CACHE_2).removeAll();
+		CacheManager manager = getInstance();
+		manager.getCache(CACHE_HIBERNATE).removeAll();
+		manager.getCache(CACHE_MEVENK_WEBAPP).removeAll();
 
-		LOG.info("Second level cache cleared for Custom Capabilities. ");
+		LOG.info("Second level cache cleared");
 
 	}
 
