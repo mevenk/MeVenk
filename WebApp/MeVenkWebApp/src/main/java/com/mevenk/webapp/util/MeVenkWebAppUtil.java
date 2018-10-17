0f73f0f9511e96f6a68acd98d45b66b205d2c727
Venkatesh Kolisetty <venkateshkolisetty@users.noreply.github.com>
2018-10-17 15:35:12 +0530

    Update WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/util/MeVenkWebAppUtil.java

--- WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/util/MeVenkWebAppUtil.java
+++ WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/util/MeVenkWebAppUtil.java
@@ -5,6 +5,7 @@ package com.mevenk.webapp.util;
 
 import static com.mevenk.webapp.util.constants.MeVenkWebAppConstants.POUND_SIGN;
 import static java.lang.System.lineSeparator;
+import static org.springframework.util.StringUtils.hasLength;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -13,12 +14,15 @@ import java.io.StringWriter;
  * @author venky
  *
  */
-public final class MeVenkWebAppUtil {
+public abstract class MeVenkWebAppUtil {
 
 	public static final String LINE_SEPARATOR = lineSeparator();
 
+	public static final IllegalStateException ILLEGAL_STATE_EXCEPTION_UTILITY_CLASS = new IllegalStateException(
+			"Utility class");
+
 	private MeVenkWebAppUtil() {
-		throw new IllegalStateException("Utility class");
+		throw ILLEGAL_STATE_EXCEPTION_UTILITY_CLASS;
 	}
 
 	/**
@@ -40,4 +44,28 @@ public final class MeVenkWebAppUtil {
 		exception.printStackTrace(new PrintWriter(stringWriter));
 		return stringWriter.toString();
 	}
+
+	/**
+	 *
+	 * @param string
+	 * @return
+	 */
+	public static boolean isStringEmptyOrNull(String string) {
+		return hasLength(string);
+	}
+
+	/**
+	 *
+	 * @param strings
+	 * @return
+	 */
+	public static boolean isAnyStringEmptyOrNull(String... strings) {
+		for (String string : strings) {
+			if (isStringEmptyOrNull(string)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
