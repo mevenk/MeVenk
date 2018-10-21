95956b83be2892dadfd3ed6b206173a1497d7e88
Venkatesh Kolisetty <venkateshkolisetty@users.noreply.github.com>
2018-10-21 20:17:14 +0530

    Update WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/util/MeVenkWebAppUtil.java
    Utility methods added

--- WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/util/MeVenkWebAppUtil.java
+++ WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/util/MeVenkWebAppUtil.java
@@ -3,12 +3,29 @@
  */
 package com.mevenk.webapp.util;
 
+import static com.mevenk.webapp.util.constants.MeVenkWebAppConstants.ANGLE_BRACKET_CLOSE;
+import static com.mevenk.webapp.util.constants.MeVenkWebAppConstants.ANGLE_BRACKET_OPEN;
+import static com.mevenk.webapp.util.constants.MeVenkWebAppConstants.COMMA_AND_SPACE;
+import static com.mevenk.webapp.util.constants.MeVenkWebAppConstants.EMPTY_STRING;
 import static com.mevenk.webapp.util.constants.MeVenkWebAppConstants.POUND_SIGN;
+import static com.mevenk.webapp.util.constants.MeVenkWebAppConstants.SQUARE_BRACKET_CLOSE;
+import static com.mevenk.webapp.util.constants.MeVenkWebAppConstants.SQUARE_BRACKET_OPEN;
 import static java.lang.System.lineSeparator;
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.HOUR;
+import static java.util.Calendar.MILLISECOND;
+import static java.util.Calendar.MINUTE;
+import static java.util.Calendar.MONTH;
+import static java.util.Calendar.SECOND;
+import static java.util.Calendar.YEAR;
+import static java.util.UUID.randomUUID;
 import static org.springframework.util.StringUtils.hasLength;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
 
 /**
  * @author venky
@@ -18,11 +35,180 @@ public abstract class MeVenkWebAppUtil {
 
 	public static final String LINE_SEPARATOR = lineSeparator();
 
+	private static final Class<?>[] baseClasses = { String.class, Byte.class, Short.class, Integer.class, Long.class,
+			Float.class, Double.class, Boolean.class };
+
+	private static final String ALL_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	private static final int INT_MAX_VALUE_LENGTH = String.valueOf(Integer.MAX_VALUE).length();
+
 	public static final IllegalAccessException ILLEGAL_ACCESS_EXCEPTION_UTILITY_CLASS = new IllegalAccessException(
 			"Utility class");
 
+	private static final Random RANDOM = new Random();
+
+	/**
+	 *
+	 * @throws IllegalAccessException
+	 */
 	private MeVenkWebAppUtil() throws IllegalAccessException {
 		throw ILLEGAL_ACCESS_EXCEPTION_UTILITY_CLASS;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public static String randomUUIDString() {
+		return randomUUID().toString();
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public static Date randomDate() {
+		return randomFutureDate();
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public static Date randomPastDate() {
+
+		Calendar calendar = Calendar.getInstance();
+		int currentYear = calendar.get(YEAR);
+		calendar.set(YEAR, currentYear - RANDOM.nextInt(10));
+		calendar.set(MONTH, currentYear - RANDOM.nextInt(11));
+		calendar.set(DAY_OF_MONTH, currentYear - RANDOM.nextInt(26));
+		calendar.set(HOUR, currentYear - RANDOM.nextInt(11));
+		calendar.set(MINUTE, currentYear - RANDOM.nextInt(59));
+		calendar.set(SECOND, currentYear - RANDOM.nextInt(59));
+		calendar.set(MILLISECOND, currentYear - RANDOM.nextInt(998));
+		return calendar.getTime();
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public static Date randomFutureDate() {
+
+		Calendar calendar = Calendar.getInstance();
+		int currentYear = calendar.get(YEAR);
+		calendar.set(YEAR, currentYear + RANDOM.nextInt(10));
+		calendar.set(MONTH, currentYear + RANDOM.nextInt(11));
+		calendar.set(DAY_OF_MONTH, currentYear + RANDOM.nextInt(26));
+		calendar.set(HOUR, currentYear + RANDOM.nextInt(11));
+		calendar.set(MINUTE, currentYear + RANDOM.nextInt(59));
+		calendar.set(SECOND, currentYear + RANDOM.nextInt(59));
+		calendar.set(MILLISECOND, currentYear + RANDOM.nextInt(998));
+		return calendar.getTime();
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public static int randomPositiveNumber() {
+		return randomPositiveNumber(5);
+	}
+
+	/**
+	 *
+	 * @param numberSize
+	 * @return
+	 */
+	public static int randomPositiveNumber(int numberSize) {
+		if (numberSize > INT_MAX_VALUE_LENGTH) {
+			numberSize = INT_MAX_VALUE_LENGTH;
+		}
+
+		StringBuilder stringBuilderRandomPositiveNumber = new StringBuilder();
+		for (int index = 0; index < numberSize; index++) {
+			stringBuilderRandomPositiveNumber.append(RANDOM.nextInt(10));
+		}
+		return Integer.valueOf(stringBuilderRandomPositiveNumber.toString());
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public static String randomString() {
+		return randomString(10);
+	}
+
+	/**
+	 *
+	 * @param requiredStringLength
+	 * @return
+	 */
+	public static String randomString(int requiredStringLength) {
+
+		StringBuilder stringBuilderRandomString = new StringBuilder();
+		for (int index = 0; index < requiredStringLength; index++) {
+			stringBuilderRandomString.append(ALL_CHARS.charAt(RANDOM.nextInt(ALL_CHARS.length() - 1)));
+		}
+		return stringBuilderRandomString.toString();
+	}
+
+	/**
+	 *
+	 * @param objects
+	 * @return
+	 */
+	public static String objectArrayAsString(Object... objects) {
+		if (objects.length <= 0) {
+			return EMPTY_STRING;
+		}
+		StringBuilder stringBuilderObjects = new StringBuilder();
+		stringBuilderObjects.append(SQUARE_BRACKET_OPEN);
+		for (Object object : objects) {
+			if (object instanceof String) {
+				stringBuilderObjects.append(object.toString() + COMMA_AND_SPACE);
+			} else {
+				stringBuilderObjects.append(
+						ANGLE_BRACKET_OPEN + object.getClass().getSimpleName() + ANGLE_BRACKET_CLOSE + COMMA_AND_SPACE);
+			}
+		}
+		stringBuilderObjects.delete(stringBuilderObjects.lastIndexOf(COMMA_AND_SPACE), stringBuilderObjects.length());
+		stringBuilderObjects.append(SQUARE_BRACKET_CLOSE);
+		return stringBuilderObjects.toString();
+	}
+
+	/**
+	 *
+	 * @param typeRequired
+	 * @param objects
+	 * @return
+	 */
+	public static String argumentsAsAppendableString(boolean typeRequired, Object... objects) {
+		StringBuilder stringBuilderObjects = new StringBuilder();
+		for (Object object : objects) {
+			if (object != null && isBaseClass(object)) {
+				stringBuilderObjects.append(POUND_SIGN + String.valueOf(object));
+			} else if (typeRequired) {
+				stringBuilderObjects.append(
+						POUND_SIGN + SQUARE_BRACKET_OPEN + object.getClass().getSimpleName() + SQUARE_BRACKET_CLOSE);
+			}
+		}
+		return stringBuilderObjects.toString();
+	}
+
+	/**
+	 *
+	 * @param object
+	 * @return
+	 */
+	private static boolean isBaseClass(Object object) {
+		for (Class<?> currentClass : baseClasses) {
+			if (object.getClass().equals(currentClass)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
