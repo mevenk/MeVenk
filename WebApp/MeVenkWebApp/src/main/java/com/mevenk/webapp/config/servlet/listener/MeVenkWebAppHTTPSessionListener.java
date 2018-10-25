bc51ab4c66e01de4cb4421110207b4008824fada
Venkatesh Kolisetty <venkateshkolisetty@users.noreply.github.com>
2018-10-25 07:07:15 +0530

    Update WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/config/servlet/listener/MeVenkWebAppHTTPSessionListener.java

--- WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/config/servlet/listener/MeVenkWebAppHTTPSessionListener.java
+++ WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/config/servlet/listener/MeVenkWebAppHTTPSessionListener.java
@@ -3,9 +3,13 @@
  */
 package com.mevenk.webapp.config.servlet.listener;
 
+import static com.mevenk.webapp.util.constants.MeVenkWebAppConstants.VERTICAL_BAR;
 import static org.apache.logging.log4j.LogManager.getLogger;
 
+import java.util.Date;
+
 import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
@@ -24,13 +28,18 @@ public class MeVenkWebAppHTTPSessionListener implements HttpSessionListener {
 	private static final Logger LOG = getLogger(MeVenkWebAppHTTPSessionListener.class);
 
 	@Override
-	public void sessionCreated(HttpSessionEvent se) {
-		LOG.info("HTTP Session Created:" + se.getSession());
+	public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+		HttpSession session = httpSessionEvent.getSession();
+		LOG.info("HTTP Session Created:" + session.getId() + VERTICAL_BAR + new Date(session.getCreationTime())
+				+ VERTICAL_BAR + session.getMaxInactiveInterval() + VERTICAL_BAR + session);
 	}
 
 	@Override
-	public void sessionDestroyed(HttpSessionEvent se) {
-		LOG.info("HTTP Session Destroyed:" + se.getSession());
+	public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+		HttpSession session = httpSessionEvent.getSession();
+		LOG.info("HTTP Session Destroyed:" + session.getId() + VERTICAL_BAR + new Date(session.getCreationTime())
+				+ VERTICAL_BAR + new Date(session.getLastAccessedTime()) + VERTICAL_BAR
+				+ session.getMaxInactiveInterval() + VERTICAL_BAR + session);
 	}
 
 }
