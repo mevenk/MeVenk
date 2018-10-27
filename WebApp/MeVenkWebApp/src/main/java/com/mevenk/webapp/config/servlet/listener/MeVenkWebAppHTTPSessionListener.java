b9d6c33c67e49d016e7a3a65bf5c72325feb41d2
Venkatesh Kolisetty <venkateshkolisetty@users.noreply.github.com>
2018-10-27 14:50:34 +0530

    Update WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/config/servlet/listener/MeVenkWebAppHTTPSessionListener.java

--- WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/config/servlet/listener/MeVenkWebAppHTTPSessionListener.java
+++ WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/config/servlet/listener/MeVenkWebAppHTTPSessionListener.java
@@ -3,10 +3,12 @@
  */
 package com.mevenk.webapp.config.servlet.listener;
 
+import static com.mevenk.webapp.config.logger.MeVenkWebAppLogger.THREAD_CONTEXT_KEY_ATTRIBUTE_SESSION_ATTRIBUTE_NAME_SESSION_ID;
 import static com.mevenk.webapp.util.constants.MeVenkWebAppConstants.VERTICAL_BAR;
 import static org.apache.logging.log4j.LogManager.getLogger;
 
 import java.util.Date;
+import java.util.Enumeration;
 
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSession;
@@ -14,6 +16,7 @@ import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.stereotype.Component;
 
 /**
@@ -32,6 +35,8 @@ public class MeVenkWebAppHTTPSessionListener implements HttpSessionListener {
 		HttpSession session = httpSessionEvent.getSession();
 		LOG.info("HTTP Session Created:" + session.getId() + VERTICAL_BAR + new Date(session.getCreationTime())
 				+ VERTICAL_BAR + session.getMaxInactiveInterval() + VERTICAL_BAR + session);
+
+		populateRequiredSessionAttributes(session);
 	}
 
 	@Override
@@ -40,6 +45,31 @@ public class MeVenkWebAppHTTPSessionListener implements HttpSessionListener {
 		LOG.info("HTTP Session Destroyed:" + session.getId() + VERTICAL_BAR + new Date(session.getCreationTime())
 				+ VERTICAL_BAR + new Date(session.getLastAccessedTime()) + VERTICAL_BAR
 				+ session.getMaxInactiveInterval() + VERTICAL_BAR + session);
+	}
+
+	/**
+	 *
+	 * @param session
+	 */
+	private void populateRequiredSessionAttributes(HttpSession session) {
+
+		populateThreadContextRequiredSessionAttributes(session);
+
+		Enumeration<String> attributeNamesHTTPSession = session.getAttributeNames();
+		String sessionAttributeName = null;
+		while (attributeNamesHTTPSession.hasMoreElements()) {
+			sessionAttributeName = attributeNamesHTTPSession.nextElement();
+			LOG.info("HTTP Session attributes created during Session initialization: {}[{}]", sessionAttributeName,
+					session.getAttribute(sessionAttributeName));
+		}
+	}
+
+	/**
+	 *
+	 * @param session
+	 */
+	private void populateThreadContextRequiredSessionAttributes(HttpSession session) {
+		ThreadContext.put(THREAD_CONTEXT_KEY_ATTRIBUTE_SESSION_ATTRIBUTE_NAME_SESSION_ID, session.getId());
 	}
 
 }
