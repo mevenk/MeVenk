0d432db53480deda876ed06364859ad4ca6a3930
Venkatesh Kolisetty <venkateshkolisetty@users.noreply.github.com>
2018-10-28 22:23:07 +0530

    Update WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/config/spring/properties/DatabaseProperties.java

--- WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/config/spring/properties/DatabaseProperties.java
+++ WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/config/spring/properties/DatabaseProperties.java
@@ -5,6 +5,7 @@ package com.mevenk.webapp.config.spring.properties;
 
 import static com.mevenk.webapp.config.spring.properties.PropertiesLoaderHelper.logPropertyDetail;
 import static com.mevenk.webapp.config.spring.properties.PropertiesLoaderHelper.validateIntegerProperty;
+import static com.mevenk.webapp.config.spring.properties.PropertiesLoaderHelper.validateStringProperty;
 
 import javax.annotation.PostConstruct;
 
@@ -30,6 +31,11 @@ public class DatabaseProperties {
 	private String propertyValueMaxPoolSize;
 	public static int maxPoolSize;
 
+	private static final String PROPERTY_NAME_DATABASE_STATISTICS_LOGGING_POLLING_CRON_EXPRESSION = "database.statistics.logging.polling.cronExpression";
+	@Value("${" + PROPERTY_NAME_DATABASE_STATISTICS_LOGGING_POLLING_CRON_EXPRESSION + "}")
+	private String propertyValueDatabaseStatisticsLoggingPollingCronExpression;
+	private static String databaseStatisticsLoggingPollingCronExpression;
+
 	@PostConstruct
 	private void loadBaseProperties() {
 
@@ -54,6 +60,33 @@ public class DatabaseProperties {
 
 		logPropertyDetail(CLASS_DATABASE_PROPERTIES, PROPERTY_NAME_DATABASE_DATA_SOURCE_MAX_POOL_SIZE, maxPoolSize);
 
+		databaseStatisticsLoggingPollingCronExpression = validateStringProperty(
+				PROPERTY_NAME_DATABASE_STATISTICS_LOGGING_POLLING_CRON_EXPRESSION,
+				propertyValueDatabaseStatisticsLoggingPollingCronExpression);
+		logPropertyDetail(CLASS_DATABASE_PROPERTIES, PROPERTY_NAME_DATABASE_STATISTICS_LOGGING_POLLING_CRON_EXPRESSION,
+				databaseStatisticsLoggingPollingCronExpression);
+
+	}
+
+	/**
+	 * @return the minPoolSize
+	 */
+	public static final int getMinPoolSize() {
+		return minPoolSize;
+	}
+
+	/**
+	 * @return the maxPoolSize
+	 */
+	public static final int getMaxPoolSize() {
+		return maxPoolSize;
+	}
+
+	/**
+	 * @return the databaseStatisticsLoggingPollingCronExpression
+	 */
+	public static final String getDatabaseStatisticsLoggingPollingCronExpression() {
+		return databaseStatisticsLoggingPollingCronExpression;
 	}
 
 }
