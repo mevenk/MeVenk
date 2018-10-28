50db18a129dab0143def8f93d0f740bf29a0c8c4
Venkatesh Kolisetty <venkateshkolisetty@users.noreply.github.com>
2018-10-28 22:02:43 +0530

    Update WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/util/expressions/MeVenkWebAppExpressions.java

---
+++ WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/util/expressions/MeVenkWebAppExpressions.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package com.mevenk.webapp.util.expressions;
+
+import static com.mevenk.webapp.util.MeVenkWebAppUtil.ILLEGAL_ACCESS_EXCEPTION_UTILITY_CLASS;
+
+/**
+ * @author venky
+ *
+ */
+public final class MeVenkWebAppExpressions {
+
+	private MeVenkWebAppExpressions() throws IllegalAccessException {
+		throw ILLEGAL_ACCESS_EXCEPTION_UTILITY_CLASS;
+	}
+
+	public static final String DATE_POLLING_CRON_EXPRESSION = "${datePollingCronExpression}";
+
+}
