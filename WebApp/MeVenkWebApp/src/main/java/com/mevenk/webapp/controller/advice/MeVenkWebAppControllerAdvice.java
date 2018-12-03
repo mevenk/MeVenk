ea6b8b5b104803fe5b0079324f5765e81d08222e
Venkatesh Kolisetty <venkateshkolisetty@users.noreply.github.com>
2018-12-03 21:51:38 +0530

    Update MeVenkWebAppControllerAdvice.java

--- WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/controller/advice/MeVenkWebAppControllerAdvice.java
+++ WebApp/MeVenkWebApp/src/main/java/com/mevenk/webapp/controller/advice/MeVenkWebAppControllerAdvice.java
@@ -5,7 +5,11 @@ package com.mevenk.webapp.controller.advice;
 
 import static com.mevenk.webapp.to.ApplicationExceptionTO.APPLICATION_ERROR_IDENTIFIER;
 import static com.mevenk.webapp.to.ApplicationExceptionTO.APPLICATION_ERROR_MESSAGE;
+import static com.mevenk.webapp.util.constants.MeVenkWebAppConstants.EXCEPTION_OCCURED_ATTRIBUTE_NAME;
 import static org.apache.logging.log4j.LogManager.getLogger;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.mevenk.webapp.service.BaseService;
 import com.mevenk.webapp.to.ApplicationExceptionTO;
@@ -29,6 +34,14 @@ public class MeVenkWebAppControllerAdvice {
 
 	private static final Logger LOG = getLogger(MeVenkWebAppControllerAdvice.class);
 
+	private static final Set<Class<? extends Throwable>> EXCEPTIONS_SKIP_SAVE_DETAIL = new HashSet<>();
+
+	static {
+
+		EXCEPTIONS_SKIP_SAVE_DETAIL.add(NoHandlerFoundException.class);
+
+	}
+
 	@Autowired
 	private BaseService baseService;
 
@@ -36,7 +49,43 @@ public class MeVenkWebAppControllerAdvice {
 	public ModelAndView mevenkWebAppExceptionHandler(Throwable throwable, HttpServletRequest request,
 			HttpServletResponse response, HttpSession session) {
 		LOG.error("Error!!-{}", throwable.getClass());
-		ModelAndView modelAndViewApplicationException = new ModelAndView("applicationException");
+		return generateModelAndView(throwable, request, session);
+
+	}
+
+	/**
+	 *
+	 * @param throwable
+	 * @param request
+	 * @param response
+	 * @param session
+	 * @return
+	 */
+	private final ModelAndView generateModelAndView(Throwable throwable, HttpServletRequest request,
+			HttpSession session) {
+
+		boolean isSkipException = EXCEPTIONS_SKIP_SAVE_DETAIL.contains(throwable.getClass());
+		String viewName = isSkipException ? "unknownError" : "applicationException";
+
+		ModelAndView modelAndViewApplicationException = new ModelAndView(viewName);
+
+		if (!isSkipException) {
+			saveExceptionDetail(throwable, request, session, modelAndViewApplicationException);
+		}
+
+		modelAndViewApplicationException.addObject(EXCEPTION_OCCURED_ATTRIBUTE_NAME, throwable);
+		return modelAndViewApplicationException;
+
+	}
+
+	/**
+	 * @param throwable
+	 * @param request
+	 * @param session
+	 * @param modelAndViewApplicationException
+	 */
+	private final void saveExceptionDetail(Throwable throwable, HttpServletRequest request, HttpSession session,
+			ModelAndView modelAndViewApplicationException) {
 		ApplicationExceptionTO applicationExceptionTO = baseService.saveApplicationException(throwable, request,
 				session);
 		LOG.info("Exception saved @ {}", applicationExceptionTO);
@@ -46,7 +95,6 @@ public class MeVenkWebAppControllerAdvice {
 				applicationExceptionTO.getApplicationErrorIdentifier());
 		modelAndViewApplicationException.addObject(APPLICATION_ERROR_MESSAGE,
 				applicationExceptionTO.getApplicationErrorMessage());
-		return modelAndViewApplicationException;
 	}
 
 }
