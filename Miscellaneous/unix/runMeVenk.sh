40b763e5fb791d059f86e03b350aaf7f9c8ab46e
Venkatesh Kolisetty <venky.svtv@live.com>
2019-04-05 06:58:32 +0530

    Linux utils

---
+++ Miscellaneous/unix/runMeVenk.sh
@@ -0,0 +1,16 @@
+
+baseDirectoryMeVenk=/home/vkolisetty/RABOTA/MeVenk
+
+nautilus $baseDirectoryMeVenk
+
+
+
+
+
+## Open Excel files directory
+#nautilus /home/vkolisetty/RABOTA/Temporary/ExcelBuilder
+
+## Open GitDiffs directory
+nautilus /home/vkolisetty/RABOTA/Temporary/GitDiffs
+
+
