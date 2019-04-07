5c8dd608f0b0b18d640ce06636cdabe6ca2c41ef
Venkatesh Kolisetty <venky.svtv@live.com>
2019-04-07 09:39:42 +0530

    Utils | GIT report | Misc changes

---
+++ Utils/MeVenkUtils/src/main/java/org/mevenk/utils/git/report/log/GitLogPrintReport.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package org.mevenk.utils.git.report.log;
+
+import static org.mevenk.utils.git.report.log.util.GitLogReportUtil.LINE_SEPARATOR;
+import static org.mevenk.utils.git.report.log.util.GitLogReportUtil.writeToStream;
+
+import java.io.OutputStream;
+import java.text.MessageFormat;
+import java.util.Date;
+import java.util.LinkedHashSet;
+
+import org.mevenk.utils.git.report.log.data.GitDiffData;
+import org.mevenk.utils.git.report.log.data.GitDiffData.ChangeType;
+import org.mevenk.utils.git.report.log.data.GitLogData;
+
+/**
+ * @author vkolisetty
+ *
+ */
+abstract class GitLogPrintReport {
+
+	/**
+	 * 
+	 * @param gitLogDatas
+	 * @param outputStream
+	 * @throws Exception
+	 */
+	static final void printLogData(LinkedHashSet<GitLogData> gitLogDatas, OutputStream outputStream) throws Exception {
+
+		writeToStream(LINE_SEPARATOR + new Date() + LINE_SEPARATOR + LINE_SEPARATOR, outputStream);
+
+		for (GitLogData log : gitLogDatas) {
+
+			writeToStream("commit " + log.getAbbreviatedCommit() + "	[" + log.getCommit() + "]" + LINE_SEPARATOR,
+					outputStream);
+			writeToStream("Author: " + log.getAuthorName() + " <" + log.getAuthorEmail() + ">" + LINE_SEPARATOR,
+					outputStream);
+			writeToStream("Date: " + log.getWhen() + LINE_SEPARATOR, outputStream);
+			writeToStream(LINE_SEPARATOR, outputStream);
+			writeToStream("	" + log.getCommitFullMessage() + LINE_SEPARATOR, outputStream);
+			writeToStream(LINE_SEPARATOR, outputStream);
+
+			for (GitDiffData diff : log.getGitDiffDatas()) {
+
+				ChangeType changeType = diff.getChangeType();
+
+				switch (changeType) {
+				case RENAME:
+					writeToStream(MessageFormat.format("{0}		{1}  {2}", changeType, diff.getOldPath(),
+							diff.getNewPath()), outputStream);
+					writeToStream(LINE_SEPARATOR, outputStream);
+					break;
+
+				default:
+					writeToStream(MessageFormat.format("{0}		{1}", changeType, diff.getNewPath()), outputStream);
+					writeToStream(LINE_SEPARATOR, outputStream);
+					break;
+				}
+
+			}
+
+			writeToStream(LINE_SEPARATOR, outputStream);
+
+		}
+
+	}
+
+}
--- Utils/MeVenkUtils/src/main/java/org/mevenk/utils/git/report/log/GitLogReportGenerator.java
+++ Utils/MeVenkUtils/src/main/java/org/mevenk/utils/git/report/log/GitLogReportGenerator.java
@@ -3,17 +3,11 @@
  */
 package org.mevenk.utils.git.report.log;
 
-import static org.mevenk.utils.git.report.log.util.GitLogReportUtil.LINE_SEPARATOR;
-import static org.mevenk.utils.git.report.log.util.GitLogReportUtil.writeToStream;
-
 import java.io.File;
 import java.io.OutputStream;
-import java.text.MessageFormat;
 import java.util.Date;
 import java.util.LinkedHashSet;
 
-import org.mevenk.utils.git.report.log.data.GitDiffData;
-import org.mevenk.utils.git.report.log.data.GitDiffData.ChangeType;
 import org.mevenk.utils.git.report.log.data.GitLogData;
 
 /**
@@ -21,53 +15,6 @@ import org.mevenk.utils.git.report.log.data.GitLogData;
  *
  */
 public class GitLogReportGenerator {
-
-	/**
-	 * 
-	 * @param gitLogDatas
-	 * @param outputStream
-	 * @throws Exception
-	 */
-	private static final void printLogData(LinkedHashSet<GitLogData> gitLogDatas, OutputStream outputStream)
-			throws Exception {
-
-		writeToStream(LINE_SEPARATOR + new Date() + LINE_SEPARATOR + LINE_SEPARATOR, outputStream);
-
-		for (GitLogData log : gitLogDatas) {
-
-			writeToStream("commit " + log.getAbbreviatedCommit() + "	[" + log.getCommit() + "]" + LINE_SEPARATOR,
-					outputStream);
-			writeToStream("Author: " + log.getAuthorName() + " <" + log.getAuthorEmail() + ">" + LINE_SEPARATOR,
-					outputStream);
-			writeToStream("Date: " + log.getWhen() + LINE_SEPARATOR, outputStream);
-			writeToStream(LINE_SEPARATOR, outputStream);
-			writeToStream("	" + log.getCommitFullMessage() + LINE_SEPARATOR, outputStream);
-			writeToStream(LINE_SEPARATOR, outputStream);
-
-			for (GitDiffData diff : log.getGitDiffDatas()) {
-
-				ChangeType changeType = diff.getChangeType();
-
-				switch (changeType) {
-				case RENAME:
-					writeToStream(MessageFormat.format("{0}		{1}  {2}", changeType, diff.getOldPath(),
-							diff.getNewPath()), outputStream);
-					writeToStream(LINE_SEPARATOR, outputStream);
-					break;
-
-				default:
-					writeToStream(MessageFormat.format("{0}		{1}", changeType, diff.getNewPath()), outputStream);
-					writeToStream(LINE_SEPARATOR, outputStream);
-					break;
-				}
-
-			}
-
-			writeToStream(LINE_SEPARATOR, outputStream);
-
-		}
-
-	}
 
 	/**
 	 * 
@@ -84,7 +31,7 @@ public class GitLogReportGenerator {
 
 		LinkedHashSet<GitLogData> generateLogReport = GitLogReport.generateLogReport(gitDir, tree,
 				abbreviatedCommitLength, maxNoOfCommits, outputStreamDiff);
-		printLogData(generateLogReport, outputStreamReport);
+		GitLogPrintReport.printLogData(generateLogReport, outputStreamReport);
 
 	}
 
@@ -104,7 +51,7 @@ public class GitLogReportGenerator {
 
 		LinkedHashSet<GitLogData> generateLogReport = GitLogReport.generateLogReport(gitDir, tree,
 				abbreviatedCommitLength, since, until, outputStreamDiff);
-		printLogData(generateLogReport, outputStreamReport);
+		GitLogPrintReport.printLogData(generateLogReport, outputStreamReport);
 
 	}
 
