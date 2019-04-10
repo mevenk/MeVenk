6593bcfda9476c8e174da71d57aaa41ea862d961
Venkatesh Kolisetty <venky.svtv@live.com>
2019-04-10 21:53:53 +0530

    Utils | Zip functions - tester

--- Utils/MeVenkUtils/src/main/java/utils/test/zip/ZipFunctionsTester.java
+++ Utils/MeVenkUtils/src/main/java/utils/test/zip/ZipFunctionsTester.java
@@ -4,6 +4,7 @@
 package utils.test.zip;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,7 +17,9 @@ import org.mevenk.utils.zip.ZipFunctions;
  */
 public class ZipFunctionsTester {
 
-	private static final String DIR_TEMPORARY = "/home/vkolisetty/RABOTA/Temporary";
+	private static final String DIRECTORY_TO_BE_ZIPPED = "/home/vkolisetty/RABOTA/Temporary/ExcelBuilder";
+	private static final String DIR_ZIP_TESTER = "/home/vkolisetty/RABOTA/Temporary/ZipTester";
+	private static final File UNZIP_DIRECTORY = new File(DIR_ZIP_TESTER + "/unzips");
 	private static final SimpleDateFormat SIMPLE_DATE_FORMAT_ZIP = new SimpleDateFormat("d-M-y_H-m-s");
 	private static final Date DATE_TODAY = new Date();
 
@@ -24,16 +27,38 @@ public class ZipFunctionsTester {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		generateZipFile();
+
+		File zipFile = generateZipFile();
+		unzipZipFile(zipFile);
 
 	}
 
-	private static void generateZipFile() throws Exception {
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	private static File generateZipFile() throws Exception {
 
-		ZipFunctions.generateZip(
-				new FileOutputStream(new File(DIR_TEMPORARY + "/ZipTester/zips/" + "ZIP_"
-						+ SIMPLE_DATE_FORMAT_ZIP.format(DATE_TODAY) + ".zip")),
-				new File(DIR_TEMPORARY + "/ExcelBuilder"), false);
+		File zipFile = new File(
+				DIR_ZIP_TESTER + "/zips/" + "ZIP_" + SIMPLE_DATE_FORMAT_ZIP.format(DATE_TODAY) + ".zip");
+		ZipFunctions.generateZip(new FileOutputStream(zipFile), new File(DIRECTORY_TO_BE_ZIPPED), false);
+
+		return zipFile;
+
+	}
+
+	/**
+	 * 
+	 * @param zipFile
+	 * @throws Exception
+	 */
+	private static void unzipZipFile(File zipFile) throws Exception {
+
+		File unzipDirectory = new File(
+				UNZIP_DIRECTORY + "/" + zipFile.getName().substring(0, zipFile.getName().length() - 4));
+		unzipDirectory.mkdirs();
+
+		ZipFunctions.unzip(new FileInputStream(zipFile), unzipDirectory);
 
 	}
 
