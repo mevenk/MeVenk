14cb87570fda0ab0f7e81a1db3b839a7c7b105a4
Venkatesh Kolisetty <venky.svtv@live.com>
2019-04-16 21:52:51 +0530

    MeVenkWebServices initial commit

---
+++ WebServices/MeVenkWebServices/src/main/java/org/mevenk/MeVenkWebServicesApplication.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.mevenk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author vkolisetty
+ *
+ */
+@SpringBootApplication
+public class MeVenkWebServicesApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(MeVenkWebServicesApplication.class, args);
+
+	}
+
+}
