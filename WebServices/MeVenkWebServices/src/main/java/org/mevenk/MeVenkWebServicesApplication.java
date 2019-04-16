7ba3904e8f0b2b92b2c6bcc48f63eae462448af2
Venkatesh Kolisetty <venky.svtv@live.com>
2019-04-16 22:28:04 +0530

    MeVenkWebServices | Sample request

--- WebServices/MeVenkWebServices/src/main/java/org/mevenk/MeVenkWebServicesApplication.java
+++ WebServices/MeVenkWebServices/src/main/java/org/mevenk/MeVenkWebServicesApplication.java
@@ -4,14 +4,24 @@
 package org.mevenk;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author vkolisetty
  *
  */
+@RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class MeVenkWebServicesApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	/**
 	 * @param args
