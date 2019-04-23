df0e5da14c1075ecd04ba35e3431787a38ea3ad0
Venkatesh Kolisetty <venky.svtv@live.com>
2019-04-23 21:20:28 +0530

    MeVenkWebServices | Jar execution with variables through script

---
+++ WebServices/executable/RunMeVenkWebServices.sh
@@ -0,0 +1,39 @@
+
+
+#!/bin/bash
+
+
+export mevenkWebServicesPropertiesFileSource="file:////home/vkolisetty/RABOTA/MeVenk/WebServices/sources/properties/mevenkWebServices.properties"
+
+
+export mevenkWebServicesLogsDirectoryLoaction="/home/vkolisetty/RABOTA/MeVenk/WebServices/Logs"
+
+
+export mevenkWebServicesLog4jSocketHost="VENKATESH-NUC"
+
+
+export mevenkWebServicesLog4jSocketPort="9898"
+
+export mevenkWebServicesBasePropertiesFileSource="file:////home/vkolisetty/RABOTA/MeVenk/WebServices/sources/properties/mevenkWebServicesBase.properties"
+
+
+export mevenkWebServicesDatabasePropertiesFileSource="file:////home/vkolisetty/RABOTA/MeVenk/WebServices/sources/properties/mevenkWebServicesDatabase.properties" 
+
+
+
+###########################################################################################################################################################
+###########################################################################################################################################################
+###########################################################################################################################################################
+###########################################################################################################################################################
+###########################################################################################################################################################
+
+
+
+
+java -jar MeVenkWebServices.jar
+
+
+
+###########################################################################################################################################################
+###########################################################################################################################################################
+
