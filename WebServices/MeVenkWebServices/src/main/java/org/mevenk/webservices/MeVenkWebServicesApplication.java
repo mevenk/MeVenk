9372b2d00b4232fcc1380792393d90eaadce6b90
Venkatesh Kolisetty <venky.svtv@live.com>
2019-04-23 07:34:36 +0530

    MeVenkWebServices | JAR plugin

--- WebServices/MeVenkWebServices/src/main/java/org/mevenk/webservices/MeVenkWebServicesApplication.java
+++ WebServices/MeVenkWebServices/src/main/java/org/mevenk/webservices/MeVenkWebServicesApplication.java
@@ -3,9 +3,14 @@
  */
 package org.mevenk.webservices;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author vkolisetty
@@ -21,6 +26,21 @@ public class MeVenkWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MeVenkWebServicesApplication.class, args);
 
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Beans:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
 	}
 
 }
