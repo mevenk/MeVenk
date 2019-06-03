6eea46d5b5cf9edb05c37e2de458f7fbe658daf2
Venkatesh Kolisetty <venky.svtv@live.com>
2019-06-03 22:55:15 +0530

    WebClient sample class

---
+++ Miscellaneous/MeVenkMiscellaneous/src/main/java/org/mevenk/rest/async/webclient/SampleRestAsyncWebClient.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.mevenk.rest.async.webclient;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+/**
+ * @author vkolisetty
+ *
+ */
+public class SampleRestAsyncWebClient {
+
+	private static void sendAsyncWebClientRequest() {
+		
+		
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		sendAsyncWebClientRequest();
+
+	}
+
+}
